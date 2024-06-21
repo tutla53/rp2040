6432a5155d94a2159ddf2a55f75fc47a8b7dc5fd
Tutla Ayatullah <ayatullah_tutla@yahoo.com>
2024-06-21 15:26:57 +0700

    update servo library

--- 2_Servo-Joystick/lib/Servo.c
+++ 2_Servo-Joystick/lib/Servo.c
@@ -1,28 +1,12 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
-#include "hardware/irq.h"
 
 #include "Servo.h"
-
-uint32_t pwm_get_wrap(uint slice_num){
-    valid_params_if(PWM, slice_num >= 0 && slice_num < NUM_PWM_SLICES);
-    return pwm_hw->slice[slice_num].top;
-}
+#include "PWMmgr.h"
 
 void pwm_set_dutyH(uint slice_num, uint chan, int d){
+    /*For Servo*/
     pwm_set_chan_level(slice_num, chan, pwm_get_wrap(slice_num) * d / 10000);
-}
-
-uint32_t pwm_set_freq_duty(uint slice_num,uint chan, uint32_t f, int d){
-    uint32_t clock = PWM_CLOCK;
-    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
-    if (divider16 / 16 == 0)
-        divider16 = 16;
-    uint32_t wrap = clock * 16 / divider16 / f - 1;
-    pwm_set_clkdiv_int_frac(slice_num, divider16/16, divider16 & 0xF);
-    pwm_set_wrap(slice_num, wrap);
-    pwm_set_chan_level(slice_num, chan, wrap * d / 100);
-    return wrap;
 }
 
 void ServoInit(Servo_t *s, uint gpio, bool invert){
--- 2_Servo-Joystick/lib/Servo.h
+++ 2_Servo-Joystick/lib/Servo.h
@@ -3,7 +3,6 @@
 
 /*PWM CONFIG*/
 #define SERVO_FREQ	50
-#define	PWM_CLOCK	125000000
 #define	MIN_PERIOD	0.5	/*ms*/
 #define	MAX_PERIOD	2.5	/*ms*/
 #define MIN_DUTY	(uint16_t)(MIN_PERIOD*10*SERVO_FREQ)
@@ -19,9 +18,7 @@ typedef struct {
 	bool invert;
 } Servo_t;
 
-uint32_t pwm_get_wrap(uint slice_num);
 void pwm_set_dutyH(uint slice_num, uint chan, int d);
-uint32_t pwm_set_freq_duty(uint slice_num,uint chan, uint32_t f, int d);
 void ServoInit(Servo_t *s, uint gpio, bool invert);
 void ServoOn(Servo_t *s);
 void ServoOff(Servo_t *s); 
